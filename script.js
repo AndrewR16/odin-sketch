6d5ebcd5b0278a638d7c37c4bafacdf1af06abb7
Andrew Rowley <andrew.rowley160@outlook.com>
2024-06-06 13:39:16 -0400

    Add generation of 16 by 16 grid

---
+++ script.js
@@ -0,0 +1,14 @@
+const GRID_CONTAINER_SIZE = 768;
+
+let gridWidth = 16;
+
+const gridContainer = document.querySelector(".gridContainer");
+
+for (let i = 0; i < Math.pow(gridWidth, 2); i++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare");
+    gridSquare.style.width = `${GRID_CONTAINER_SIZE / 16}px`;
+    gridSquare.style.height = `${GRID_CONTAINER_SIZE / 16}px`;
+    
+    gridContainer.appendChild(gridSquare);
+}
