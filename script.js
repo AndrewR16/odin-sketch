9eaf4f00dc75bbbd5657f214743b6a1f0b30bfd1
Andrew Rowley <andrew.rowley160@outlook.com>
2024-06-06 13:50:12 -0400

    Add hover effect to grid squares

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ for (let i = 0; i < Math.pow(gridWidth, 2); i++) {
     gridSquare.classList.add("gridSquare");
     gridSquare.style.width = `${GRID_CONTAINER_SIZE / 16}px`;
     gridSquare.style.height = `${GRID_CONTAINER_SIZE / 16}px`;
-    
+
     gridContainer.appendChild(gridSquare);
+    addHoverEffect();
+}
+
+function addHoverEffect() {
+    document.querySelectorAll(".gridSquare").forEach((el) => {
+        el.addEventListener("mouseover", (event) => {
+            let element = event.target;
+
+            element.style.backgroundColor = "black";
+        })
+    });
 }
